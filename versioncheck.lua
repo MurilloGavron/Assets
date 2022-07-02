1eeec86240794b9d7c3c30a45a18b122ae3c6749
MurilloGavron <77905220+MurilloGavron@users.noreply.github.com>
2022-07-02 14:10:55 -0300

    Update versioncheck.lua

--- versioncheck.lua
+++ versioncheck.lua
@@ -10,7 +10,7 @@ function checkVersion(err,responseText, headers)
 		print("\n"..resourceName.." is outdated, should be:\n"..responseText.."is:\n"..curVersion.."\nplease update it from https://github.com"..updatePath.."")
 		print("\n###############################")
 	elseif tonumber(curVersion) > tonumber(responseText) then
-		print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update ( or downgrade? )")
+		print("You somehow skipped a few versions of "..resourceName.." or the git wentddddd offline, if it's still online i advise you to update ( or downgrade? )")
 	else
 		print("\n"..resourceName.." is up to date, have fun!")
 	end
